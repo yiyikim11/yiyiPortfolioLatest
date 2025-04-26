832ffc6a1a59678f7cbdfc522491e349e33368e4
yiyikim <yiyikim11@gmail.com>
2025-04-26 14:24:24 +0700

    working on body nth new just commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,6 @@ import React from 'react'
 const Body = () => {
   return (
     <main>
-
-        <div className='mt-12'>
-          <h2 className='font-bold text-lg'>Work Experience</h2>
-        </div>
-
         <div className='mt-12'>
           <h2 className='font-bold text-lg'>Education</h2>
         </div>
